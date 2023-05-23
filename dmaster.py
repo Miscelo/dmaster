b2949da94763a134f8939bd4bdf240d09e32ff1b
Miscelo <miscelo@tutanota.com>
2023-05-23 20:22:15 +0200

    openports function from dports will be checked in future with services and go to report.

--- dmaster.py
+++ dmaster.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 import diskspace
+import dports
+import goodies
+
 
 '''
 Main program that will handle a menu.
 '''
 
+def main():
+    print("************* DMASTER ***************")
+    diskspace.disk_usage()
+    openports = dports.openports()
+    print(openports)
 
+
+
+if __name__ == "__main__":
+    main()
