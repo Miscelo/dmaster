ef1a4788a03b0d95d0bac61a27ee1c0b4cd15424
Miscelo <miscelo@tutanota.com>
2023-05-23 20:19:42 +0200

    introduce config dictionary to get values.

--- diskspace.py
+++ diskspace.py
@@ -1,57 +1,59 @@
 
 import shutil
 import os.path
-import logging
+import goodies
 from datetime import datetime
 
-now = datetime.now()
 
-
-
-
-""" FunctionReturn True, if there is enough disk space"""
-def disk_usage(path, min_emergency, min_critical, now, statsfile):
-    # Size, Used , Available, Used %
-    total, used, free = shutil.disk_usage(path)
-    #Cambiamos de Bytes a GB
-    totalGB = round(total / 2**30, 2)
-    usedGB = round(used / 2**30, 2)
-    freeGB = round(free / 2**30, 2)
-    used_percent = round(100 * (used / 2**30) / (total / 2**30), 2)
-    timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
-    stats = timestamp+","+path+","+str(totalGB)+","+str(usedGB)+","+str(freeGB)+","+str(used_percent)
-    writeDiskStats(statsfile, stats)
-
-    if (100 - used_percent) < min_emergency:
-        print("Emergency alert! Diskspace {}% full.".format(path, used_percent))
-        return False
-    elif (100 - used_percent) < min_critical:
-        print("Emergency alert! Diskspace {}% full.".format(path, used_percent))
-        return False
-    return True
-
-
+# Function creates a file for diskusage statistics. The first line have column-info.
 def writeDiskStats(statsfile, stats):
+    # First time executed, in statsfile will be added first line with infos.
     if not os.path.isfile(statsfile):
         try:
             with open(statsfile, "w") as fobj:
-                fobj.write("Date & Time, Size, Used, Available, Used in %\n")
+                fobj.write("# Date & Time, Size, Used, Available, Used in %\n")
+                fobj.write(stats)
+        except FileNotFoundError:
+            goodies.log("dmaster.diskspace", "ERROR", "File for stats not created.")
         except Exception:
-            print("Error to access to file {}. Please check permissions.".format(statsfile))
-            logging.warning("ERROR dmaster: diskspace - can not create file for stats.")
+            goodies.log("dmaster.diskspace", "ERROR", "File for stats not created.")
     else:
         try:
             with open(statsfile, "a+") as fobj:
                 fobj.write(stats)
         except Exception:
-            print("Error to access to file {}. Please check permissions.".format(statsfile))
-            logging.warning("ERROR dmaster: diskspace - can not write stats to file.")
+            goodies.log("dmaster.diskspace", "ERROR", "Can not write stats to file diskspace.dat.")
 
 
 
+# Function check usage of disk and passed it to a statistic file. If the limits of diskusage are broken, given by
+# dmaster.conf file, there will be a log and later soon an email alert.
+def disk_usage():
+    statsfile = "diskusage.dat"
+    now = datetime.now()
+    configs = goodies.getConfigData()
+    max_critical = float(configs['disk_usage_critical'])
+    max_warning = float(configs['disk_usage_warning'])
+    pathlist = configs['directories']
+    for dir in pathlist:
+        # Size, Used , Available, Used %
+        total, used, free = shutil.disk_usage(dir)
+        #Cambiamos de Bytes a GB
+        totalGB = round(total / 2**30, 2)
+        usedGB = round(used / 2**30, 2)
+        freeGB = round(free / 2**30, 2)
+        used_percent = round(100 * (used / 2**30) / (total / 2**30), 2)
+        timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
+        stats = f'{timestamp},{dir},{totalGB},{usedGB},{freeGB},{used_percent}\n'
+        # Pass stats to a file
+        writeDiskStats(statsfile, stats)
+        message = f"Diskspace on {dir} to {used_percent}% full."
+        if (100 - used_percent) < (100 - max_critical):
+            goodies.log("dmaster.diskspace", "CRITICAL", message)
+            # send mail with 50
+        if (100 - max_critical) < (100 - used_percent) and (100 - used_percent) < (100 - max_warning):
+            goodies.log("dmaster.diskspace", "WARNING", message)
+            # send mail with 30
 
-path = "/boot/"
-min_emerg = 2
-min_critical = 10
-a = disk_usage(path, min_emerg, min_critical, now, "diskspace.dat")
+
 
